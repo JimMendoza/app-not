a47ca2577272bc4bc81fc0cea0c252ca3868d408
Jim Mendoza <jim.oscar.mendoza.espinoza@gmail.com>
2026-03-27 11:13:58 -0500

    Limpia housekeeping final del frontend

--- lib/config/constants/app_metadata.dart
+++ lib/config/constants/app_metadata.dart
@@ -1,6 +1,6 @@
 class AppMetadata {
   // Keep this value aligned with pubspec.yaml.
-  static const String version = '1.0.1';
+  static const String version = '1.0.0+1';
 
   const AppMetadata._();
 }
--- lib/features/auth/infrastructure/infrastructure.dart
+++ lib/features/auth/infrastructure/infrastructure.dart
@@ -7,6 +7,4 @@ export 'repositories/entidad_repository_impl.dart';
 export 'mappers/user_mapper.dart';
 export 'mappers/entidad_mapper.dart';
 
-export 'errors/auth_errors.dart';
-
 export 'inputs/inputs.dart';
